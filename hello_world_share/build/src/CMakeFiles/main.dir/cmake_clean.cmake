9636c708f9ad5204bcb6e8cc096c1902119a70b3
Browny Lin <browny_lin@htc.com>
2011-10-14 18:50:26 +0800

    add some practices

---
+++ hello_world_share/build/src/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/main.dir/main.o"
+  "main.pdb"
+  "main"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
